02e2c0c84e79fe0164bb24408fd114c16302bc80
lckei <lc.kei06@gmail.com>
2021-12-07 18:38:22 +0800

    采用shadow dom 重写并精简样式

--- js/content.js
+++ js/content.js
@@ -1,39 +1,67 @@
-var oDiv = document.createElement('div');
-document.body.appendChild(oDiv);
-var image = document.createElement('img');
-oDiv.appendChild(image);
-oDiv.id = 'div1';
-oDiv.style.position = 'absolute';
-oDiv.style.display = 'none';
-image.className = '_btn_';
-image.src = chrome.extension.getURL("btn.png");
-image.style.height = '28px';
-image.style.width = '28px';
-var pos = { left: 0, top: 0 };
+var pos = { left: 0, top: 0 }; //__btn___ 座标
 //鼠标拖动div
 var mousePosition;
 var offset = [0, 0];
 var wheader;
 var isDown = false;
 
-var win = document.createElement('div');
-win.innerHTML = `<div class="ax-panel ax-border ax-radius" style="width:360px;min-height:150px">
-        <div id="wheader" class="ax-panel-header ax-row">
-            <div class="ax-col"><span><i>o</i></span></div>
-            <span id="wclosebtn" class="ax-operate ax-panel-close"><i >x</i></span>
-        </div>
-        <div class="ax-break-line"></div>
-        <div class="ax-panel-body ax-article"><textarea  id="dtext"></textarea>
-        </div>
-    </div>`;
-document.body.appendChild(win);
-win.id = "div2";
-win.style.position = 'absolute';
-win.style.display = 'none';
+//--------1初始化shadow Dom---------//
 
+let popwin = document.createElement('div');
+popwin.style.cssText = "all: initial;" //重置样式，避免全局的样式影响
+const shadowRoot = popwin.attachShadow({ mode: 'open' });
+popwin.shadowRoot;
+popwin.shadowRoot.host;
+let style = document.createElement('style');
+style.innerHTML = "@import '" + chrome.extension.getURL("/css/custom.css") + "';";
+shadowRoot.appendChild(style);
 
-wheader = document.getElementById("wheader");
-//查找字串
+//-------------- 1.构造划词图标 ---------//
+let divRoot = document.createElement('div');
+divRoot.id = "root";
+let divRootApp = document.createElement('div');
+divRootApp.id = "app";
+divRoot.appendChild(divRootApp);
+let divRootAppSelectext = document.createElement('div'); //btn 默认不显示,通过控制div diplay none block 来关闭|显示 
+divRootAppSelectext.id = "selectext";
+divRootAppSelectext.style.position = "absolute"
+divRootAppSelectext.style.display = "none";
+divRootApp.appendChild(divRootAppSelectext);
+let divRootAppSelectextBtn = document.createElement('button');
+divRootAppSelectextBtn.className = "__btn__";
+divRootAppSelectext.appendChild(divRootAppSelectextBtn);
+divRootAppSelectextBtn.style.background = "url(" + chrome.extension.getURL("btn.png") + ") center center no-repeat";
+divRootAppSelectextBtn.style.width = "24px";
+divRootAppSelectextBtn.style.height = "24px";
+divRootAppSelectextBtn.style.border = "none";
+
+shadowRoot.appendChild(divRoot);
+
+//-------------- 2.构造弹出窗体 ------------//
+
+let divPanel = document.createElement('div');
+divPanel.id = "divPanel"
+divPanel.style.display = "none"; //默认不显示，btn触发则显示
+divPanel.style.position = "absolute";
+divPanel.style.width = "320px";
+divPanel.style.height = "150px";
+
+divPanel.innerHTML = `
+<div class="custom-panel custom-radius">
+   <div id="panelHeader" class="panels-heading custom-row">
+      <div class="custom-col"></div>
+            <span id="winClosebtn" class="custom-padding"><i  class="custom-icon">x</i></span>
+   </div>
+    <div class="break-line"></div>
+   <div class="panel-body">
+	<textarea id="dtext"></textarea>
+    </div>
+</div>
+`
+divRootApp.appendChild(divPanel);
+document.documentElement.appendChild(popwin);
+
+//2D字串查找（字典中数组）
 function indexOf2dArray(array2d, itemtofind) {
     index = [].concat.apply([], ([].concat.apply([], array2d))).indexOf(itemtofind);
     if (index === -1) { return false; }
@@ -52,100 +80,92 @@ function selectText() {
     }
 };
 
+//显示 div plane
 function showTopWindows(text, pos) {
     var newText = "";
     var charText = "";
     //逐字侧边加注音
-    for (var i = 0; i < text.length; i++) {
-        var res = indexOf2dArray(arr, text[i]);
-        if (res) {
-            var charIndex = res[0];
-            charText = "（" + arr[charIndex][2] + "）";
-        } else {
-            charText = "";
+    if (text.length > 0) {
+        for (var i = 0; i < text.length; i++) {
+            var res = indexOf2dArray(arr, text[i]);
+            if (res) {
+                var charIndex = res[0];
+                charText = "（" + arr[charIndex][2] + "）";
+            } else {
+                charText = "";
+            }
+            newText += text[i] + charText;
         }
-        // console.log(charIndex, charText);
-        newText += text[i] + charText;
     }
-    var d = win;
-    if (document.body.contains(d)) {
-        var dd = document.getElementById("dtext");
-        dd.value = newText;
-        d.style.display = 'block';
-        dd.style.display = 'block';
-        d.style.left = pos.left + "px";
-        d.style.top = pos.top + "px";
-    }
+    var textareaTxt = shadowRoot.getElementById("dtext");
+    textareaTxt.value = newText;
+    divPanel.style.display = "block";
+    divRootAppSelectext.style.display = "none";
+    divPanel.style.left = pos.left + "px";
+    divPanel.style.top = pos.top + "px";
+
 };
 
+//隐藏div plane
 function hiddenTopWindows() {
-    var d = win;
-    if (document.body.contains(d)) {
-        d.style.display = 'none';
-    }
+    divPanel.style.display = 'none';
 };
-//计算当前页面鼠标相对page的坐标
+//计算当前页面鼠标相对page的坐标,用于divPannel 拖动
 document.addEventListener("mousemove", function(event) {
     if (isDown) {
         mousePosition = {
             x: event.clientX,
             y: event.clientY
         };
-        div2.style.left = (mousePosition.x + offset[0]) + 'px';
-        div2.style.top = (mousePosition.y + offset[1]) + 'px';
+        divPanel.style.left = (mousePosition.x + offset[0]) + 'px';
+        divPanel.style.top = (mousePosition.y + offset[1]) + 'px';
     }
 });
 
-document.addEventListener("mouseup", function(ev) {
-    isDown = false;
-    var ev = ev || window.event;
-    pos.left = ev.pageX + 15;
-    pos.top = ev.pageY - 5;
-    if (selectText().length >= 1) {
-        //console.log(oDiv.style.left, oDiv.style.top, "offsetX: " + ev.offsetX, "offsetY: " + ev.offsetY, "pageX: " + ev.pageX, "pageY: " + ev.pageY, "clientX: " + ev.clientX, "clientY: " + ev.clientY);
-
-        setTimeout(function() { //定时器用于延迟加载否则如果选中文字太快,图片跟不上
-
-            oDiv.style.left = pos.left + 'px';
-            oDiv.style.top = pos.top + 'px';
-            oDiv.style.display = 'block';
-            image.style.display = 'block';
-        }, 100);
-    } else {
-        oDiv.style.display = 'none';
-        image.style.display = 'none';
-    }
-
-});
-
+//划词 btn 追随显示
+document.addEventListener("mouseup", (ev) => {
+        isDown = false;
+        var ev = ev || window.event;
+        pos.left = ev.pageX + 15;
+        pos.top = ev.pageY - 5;
+        if (selectText().length >= 1) {
+            setTimeout(() => {
+                divRootAppSelectext.style.left = pos.left + 'px';
+                divRootAppSelectext.style.top = pos.top + 'px';
+                divRootAppSelectext.style.display = 'block';
+            }, 100);
+        } else {
+            divRootAppSelectext.style.display = 'none';
+        }
+    })
+    //点击页面任意地方，取消btn显示
 document.onclick = function(ev) {
-
-    var ev = ev || window.event;
-
+    var ev = ev || window.event
     ev.cancelBubble = true;
-    oDiv.style.display = 'none';
-    image.style.display = 'none';
+    divRootAppSelectext.style.display = 'none';
+}
 
-};
-
-oDiv.onclick = function(ev) {
-    var ev = ev || window.event;
-    oDiv.style.display = 'none';
-    image.style.display = 'none';
+//点击btn 显示plane
+divRootAppSelectextBtn.addEventListener("click", (ev) => {
+    // console.log("clicked from btn");
+    divRootAppSelectext.style.display = 'none';
     showTopWindows(selectText(), pos);
     ev.preventDefault();
-};
+});
+
 //关闭windows
-var tid = document.getElementById("wclosebtn");
-tid.onclick = function(ev) {
+var winCloseBtn = shadowRoot.getElementById("winClosebtn");
+winCloseBtn.addEventListener("click", () => {
     hiddenTopWindows();
-};
+});
+
+var panelHeader = shadowRoot.getElementById("panelHeader")
 
 //移动div
-wheader.addEventListener('mousedown', function(ev) {
+panelHeader.addEventListener('mousedown', function(ev) {
     isDown = true;
     offset = [
-        div2.offsetLeft - ev.clientX,
-        div2.offsetTop - ev.clientY
+        divPanel.offsetLeft - ev.clientX,
+        divPanel.offsetTop - ev.clientY
     ];
 }, true);
